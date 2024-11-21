37a75f3f0e0c0c3dd68b66f8bab989f5ffb75efd
NateCal715 <157073389+NateCal715@users.noreply.github.com>
2024-11-21 05:07:20 +0000

    modified:   Graph.java 	modified:   ProblemSolutions.java

--- ProblemSolutions.java
+++ ProblemSolutions.java
@@ -1,6 +1,6 @@
 /******************************************************************
  *
- *   ADD YOUR NAME / SECTION NUMBER HERE
+ *   NATE CALDERON / SECTION 001
  *
  *   This java file contains the problem solutions of canFinish and
  *   numGroups methods.
